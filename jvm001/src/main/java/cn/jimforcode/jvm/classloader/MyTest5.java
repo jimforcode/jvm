9211bf923005ca6da3f7ccc5a26fdfd4bac37450
jimforcode <jimforcode@gmail.com>
2018-05-03 09:44:33 +0800

    init

--- jvm001/src/main/java/cn/jimforcode/jvm/classloader/MyTest5.java
+++ jvm001/src/main/java/cn/jimforcode/jvm/classloader/MyTest5.java
@@ -1,4 +1,4 @@
-package cn.jimforcode.jvm.classloader;
+﻿package cn.jimforcode.jvm.classloader;
 
 import java.util.UUID;
 
@@ -6,7 +6,7 @@ import java.util.UUID;
  * Created by jim on 2018/5/2.
  * 常量在编译阶段会存入到调用
  * 这个final常量的 方法所在类的常量池当中
- *本质上，调用类并没有直接引用到定义常量的类，因此不会触发
+ * 本质上，调用类并没有直接引用到定义常量的类，因此不会触发
  * 定义常量的类初始化
  *
  * 注意：这里指的是将常量存放到了MyTest4的常量池当中，之后MyTest4与
